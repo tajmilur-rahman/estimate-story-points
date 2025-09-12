373a65eea3906e1783bdb7212bf61970323af640
Lamyea Maha <lamyeamaha@gmail.com>
2025-09-12 17:53:07 -0600

    use summarizer.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from retriever_def import *
 from generator_def import *
+from summarizer_def import *
 from sklearn.metrics import mean_absolute_error, median_absolute_error
 import numpy as np
+import json
+import re
 
 
-
+#summarize descriptions
+summarized_file = summarize_csv("DM_deep-se.csv")
 
 # Load dataset
-train_df, test_df = load_and_prepare_data("summarized_aloy2.csv")
+train_df, test_df = load_and_prepare_data(summarized_file)
 
 #create embeddings
 index = create_embeddings(train_df)
@@ -15,6 +19,8 @@ index = create_embeddings(train_df)
 
 predictions = []
 actual = []
+similar_tasks_records = []
+results = []
 #Loop through all test tasks
 for i, test_task in test_df.iterrows():
     similar_tasks, sim_scores = retrieve_similar_tasks(index, train_df, test_task['text'])
@@ -24,29 +30,40 @@ for i, test_task in test_df.iterrows():
         task_info = {
             "rank": i + 1,
             "similarity_score": float(score),
-            "title": train_df.loc[idx, "title"],
-            "description": train_df.loc[idx, "summarized_description"],
-            "story_point": train_df.loc[idx, "storypoint"]
+            "title": str(train_df.loc[idx, "title"]),
+            "description": str(train_df.loc[idx, "summarized_description"]),
+            "story_point": str(train_df.loc[idx, "storypoint"])
         }
 
         sim_tasks.append(task_info)
         
 
-        # print(f"\nTop {i+1} Similar Task (Score: {score:.4f}):")
-        # print("Title:", train_df.loc[idx, "title"])
-        # print("Description:", train_df.loc[idx, "summarized_description"])
-        # print("Story Point:", train_df.loc[idx, "storypoint"])
-
     sp = generate(sim_tasks, test_task)
+    results.append(sp)
 
-    # Try to extract the numeric value (if LLM outputs text like "story point: 3")
     try:
-        sp_num = int(''.join(filter(str.isdigit, str(sp))))
+        match = re.search(r"\d+(\.\d+)?", str(sp))  
+        if match:
+            sp_num = float(match.group()) 
+        else:
+            sp_num = None
     except:
         sp_num = None
+
     predictions.append(sp_num)
+    actual.append(test_task["storypoint"])
+    similar_tasks_records.append(json.dumps(sim_tasks, ensure_ascii=False))
     
-    actual.append(task_info["story_point"])
+
+
+# add the actual sp and predicted sp to the dataset
+test_df["actual_sp"] = actual
+test_df["predicted_sp"] =  predictions
+test_df["results"] = results
+test_df["similar_tasks"] = similar_tasks_records
+test_df.to_csv("predictions.csv", index=False)
+
+
 
 mae = mean_absolute_error(actual, predictions)
 medae = median_absolute_error(actual, predictions)
